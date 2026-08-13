02bd50a7b22428e3ca29ce7ff1bd04a3caf8735d
grebking <growqqstock@gmail.com>
2026-08-13 03:52:24 +0200

    Initial commit - Coming Soon page

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-6xl font-light text-gray-800 tracking-wide mb-4">
+          Coming Soon
+        </h1>
+        <p className="text-xl text-gray-600 font-light">
+          The Future of Decentralized Gaming
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
